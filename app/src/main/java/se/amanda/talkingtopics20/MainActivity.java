86515b8ea34ea4d67fd3fc2f0947bec02221df73
Mandizyao <amandizz_96@hotmail.com>
2024-09-16 16:31:12 +0200

    First view works.

--- app/src/main/java/se/amanda/talkingtopics20/MainActivity.java
+++ app/src/main/java/se/amanda/talkingtopics20/MainActivity.java
@@ -7,20 +7,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
+        RecyclerView recyclerView = findViewById(R.id.category);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        List<Topic> topicsList = new ArrayList<>();
+        topicsList.add(new Topic("Fåglar", R.drawable.bird));
+        topicsList.add(new Topic("Cupcakes", R.drawable.cupcake));
+        topicsList.add(new Topic("Delfiner",  R.drawable.delfinen));
+        topicsList.add(new Topic("Havet", R.drawable.sea));
+        topicsList.add(new Topic("Manet", R.drawable.manet));
+        topicsList.add(new Topic("Festival", R.drawable.festival));
+        topicsList.add(new Topic("Basket", R.drawable.basket));
+        topicsList.add(new Topic("Kaffe", R.drawable.coffee));
+        topicsList.add(new Topic("Party", R.drawable.party));
+        topicsList.add(new Topic("Pengar", R.drawable.money));
+
+        TopicAdapter adapter = new TopicAdapter(topicsList);
+        recyclerView.setAdapter(adapter);
 
     }
 }
--- app/src/main/java/se/amanda/talkingtopics20/TopicAdapter.java
+++ app/src/main/java/se/amanda/talkingtopics20/TopicAdapter.java
@@ -1,4 +1,55 @@
 package se.amanda.talkingtopics20;
 
-public class TopicAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class TopicAdapter extends RecyclerView.Adapter<TopicAdapter.TopicViewHolder> {
+
+    private List<Topic> topicList;
+
+    public TopicAdapter(List <Topic> topicsList) {
+        this.topicList = topicsList;
+    }
+
+
+    @NonNull
+    @Override
+    public TopicAdapter.TopicViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.category_cards, parent, false);
+        return new TopicViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TopicAdapter.TopicViewHolder holder, int position) {
+        Topic topic = topicList.get(position);
+        holder.topicImage.setImageResource(topic.getImageId());
+        holder.topicTitle.setText(topic.getTitle());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return topicList.size();
+    }
+
+    public static class TopicViewHolder extends RecyclerView.ViewHolder {
+        ImageView topicImage;
+        TextView topicTitle;
+
+        public TopicViewHolder (View itemView) {
+            super(itemView);
+            topicImage = itemView.findViewById(R.id.topicImage);
+            topicTitle = itemView.findViewById(R.id.topicTitle);
+
+        }
+    }
 }
